e43354aa43c5d227f5a5038e3444e27629c6fc7d
AidanaAlmaz07 <133850971+AidanaAlmaz07@users.noreply.github.com>
2024-10-15 16:27:09 -0500

    Created login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path='/login' Component={LoginPage}/>
+      </Routes>
     </Router>
   );
 }
