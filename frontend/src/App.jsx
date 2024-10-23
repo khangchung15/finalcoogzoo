add5cfe5c521aa808bc3ea919eac325f1d02edc8
Alex <86182397+alehxs@users.noreply.github.com>
2024-10-23 17:49:31 -0500

    fix subdomain stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,32 +2,32 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import Animals from './pages/animals';
+import Animals from './pages/Animals';
 import Exhibits from './pages/Exhibits';
 import Tickets from './pages/Tickets';
 import Membership from './pages/Membership';
-import Events from './pages/events';
-import Home from './pages/home';
+import Events from './pages/Events';
+import Home from './pages/Home';
 import PrivateRoutes from './components/PrivateRoutes';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/coog-zoo">
       <Navbar />
       <Routes>
 
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />  {/* This will now be /coog-zoo/ */}
 
-        <Route path='/animals' element={<Animals />} />
-        <Route path='/exhibits' element={<Exhibits />} />
+        <Route path='/animals' element={<Animals />} />  {/* /coog-zoo/animals */}
+        <Route path='/exhibits' element={<Exhibits />} />  {/* /coog-zoo/exhibits */}
 
         <Route element={<PrivateRoutes />}>
-          <Route path='/tickets' element={<Tickets />} />
-          <Route path='/membership' element={<Membership />} />
+          <Route path='/tickets' element={<Tickets />} />  {/* /coog-zoo/tickets */}
+          <Route path='/membership' element={<Membership />} />  {/* /coog-zoo/membership */}
         </Route>
 
-        <Route path='/events' element={<Events />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/events' element={<Events />} />  {/* /coog-zoo/events */}
+        <Route path='/login' element={<Login />} />  {/* /coog-zoo/login */}
 
       </Routes>
     </Router>
