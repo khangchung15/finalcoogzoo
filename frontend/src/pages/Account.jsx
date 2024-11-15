39e174361c10027d6fcc5c48febc6b55c24ef2f2
Haha <kmchung@cougarnet.uh.edu>
2024-11-15 17:56:30 -0600

    fix Account.jsx?

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -19,7 +19,7 @@ const Account = () => {
     const fetchProfileData = async () => {
       try {
         const response = await fetch(
-          `http://localhost:5000/profile?email=${encodeURIComponent(userEmail)}&type=${encodeURIComponent(displayRole)}`,
+          `https://coogzootestbackend-phi.vercel.app/profile?email=${encodeURIComponent(userEmail)}&type=${encodeURIComponent(displayRole)}`,
           {
             method: 'GET',
             headers: {
